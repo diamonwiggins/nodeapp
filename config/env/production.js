de168f86a3b205d499a3eb84c78da581ba900ceb
diamonwiggins <diamonw757@gmail.com>
2018-04-09 13:18:34 -0400

    init commit

---
+++ config/env/production.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Expose
+ */
+
+module.exports = {
+  db: process.env.MONGOHQ_URL,
+  facebook: {
+    clientID: process.env.FACEBOOK_CLIENTID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: 'https://nodeapp.cs.odu.edu/auth/facebook/callback'
+  },
+  twitter: {
+    clientID: process.env.TWITTER_CLIENTID,
+    clientSecret: process.env.TWITTER_SECRET,
+    callbackURL: 'https://nodeapp.cs.odu.edu/auth/twitter/callback'
+  },
+  github: {
+    clientID: process.env.GITHUB_CLIENTID,
+    clientSecret: process.env.GITHUB_SECRET,
+    callbackURL: 'https://nodeapp.cs.odu.edu/auth/github/callback'
+  },
+  linkedin: {
+    clientID: process.env.LINKEDIN_CLIENTID,
+    clientSecret: process.env.LINKEDIN_SECRET,
+    callbackURL: 'https://nodeapp.cs.odu.edu/auth/linkedin/callback'
+  },
+  google: {
+    clientID: process.env.GOOGLE_CLIENTID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: 'https://nodeapp.cs.odu.edu/auth/google/callback'
+  }
+};
